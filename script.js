fe603b7ba0b76dfbd01b7acdaae618a677384ab2
otienosteve <steveotieno701@gmail.com>
2022-03-22 15:00:43 +0300

    added external javascript file

---
+++ script.js
@@ -0,0 +1,17 @@
+let year= new Date()
+year=year.getFullYear();
+document.getElementById('year').textContent=year
+
+document.addEventListener('click',(navbar)=>{
+    var nav= document.getElementByID("navbar")
+    nav.style.backgroundColor="green"
+}
+)
+document.getElementById("pop").addEventListener("click", ()=>{
+    document.getElementById("links2").style.display="block"
+    document.getElementById("cancl").style.display="block"
+})
+document.getElementById("cancl").addEventListener('click',()=>{
+    document.getElementById("links2").style.display="none"
+    document.getElementById("cancl").style.display="none"
+})
