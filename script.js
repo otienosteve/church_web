1b3ccb7f9460d7d46a9e0975b261448729dee297
otienosteve <steveotieno701@gmail.com>
2022-09-18 12:15:29 +0300

    Add Former Priest Section Slider Initial SetUp

--- script.js
+++ script.js
@@ -16,3 +16,33 @@ document.getElementById("pop").addEventListener("click", ()=>{
 //     document.getElementById("cancl").style.display="none"
 // })
 const carouselItems=document.querySelectorAll('div.preist-info')
+const priestCont=document.querySelectorAll(".former-priest-cont")
+// console.log(priestCont)
+priestCont[2].style.height="100%"
+
+
+function Clear(){
+    priestCont.forEach(elem=>{
+        elem.style.display='none'
+       })
+}
+let left=document.querySelector('.left')
+let right=document.querySelector('.right')
+
+let main=document.querySelector('.main-priest')
+console.log(left,right,main)
+setInterval(()=>{
+    clear()
+ appendImages()
+},3000)
+
+function appendImages(){
+    for(var f=0; f<=priestCont.length-1;f++){
+        console.log(f)
+        priestCont[f].style.display='block'
+       
+left.append(priestCont[f])
+// main.append(priestCont[f])
+// right.append(priestCont[f])
+    }
+}
